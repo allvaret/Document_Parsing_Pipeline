097740513ce6757c516acd618e385e213c09a219
Alvaro <alvarodanko032@gmail.com>
2026-04-09 22:12:29 -0300

    Texto padrão

--- tests/test_dect_summary.py
+++
@@ -1,9 +0,0 @@
-import extractor.summary_detector
-from extractor.summary_detector import detect_summary
-from extractor.parsers.decomp_pdf import extract_text_atoms
-
-path = "D:/Projetos/Projetoes/SmartLazys/finance-data-platform/assets/BR_PT Demonstrações Financeiras 3T25.pdf"
-result = extract_text_atoms(path)
-
-atacht = detect_summary(result)
-print(atacht)
