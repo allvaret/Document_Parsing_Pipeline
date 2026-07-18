d49861a8c124bcf21f18e40f5bfbcfa834fb2e60
Alvaro <alvarodanko032@gmail.com>
2026-07-18 18:07:30 -0300

    Adicao de resumo offline as possibilidades

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-from LLM.summarizer import summarize
+from LLM.summarize_pipeline import summarizer
 from extractor.parser_pipeline import parse_document
-from extractor.section.build_section import serialize_for_llm
+from extractor.section.json_pipeline import serialize
 from utils.save_results import save_output
 
 INPUT_FILE = ""
@@ -13,14 +13,15 @@ def main(pdf_path: str | None = None):
 
     sections = parse_document(pdf)
 
-    sections_json = serialize_for_llm(sections)
+    sections_json = serialize(sections)
 
-    summary = summarize(sections_json)
+    summary = summarizer(sections_json)
 
     save_output(sections_json, pdf, "sections")
     save_output(summary, pdf, "summary")
 
     print(summary)
+
 
 if __name__ == "__main__": 
     main(INPUT_FILE)
