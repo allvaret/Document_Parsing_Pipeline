65b680a1eea6a69071a2dcaf1e75b40576dfe16c
Alvaro <alvarodanko032@gmail.com>
2026-08-13 18:10:35 -0300

    Feat: Devolucao de status code, acesso a metodo e premissas; Adicao de examples em premissa

--- src/site_backend/main.py
+++ src/site_backend/main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
 from src.site_backend.calculadora.orquestrador import CalculadoraTetoDePreco
 from src.site_backend.models import MetodoPreco, PremissasFactory, ResultadoCalculo
 
@@ -21,7 +20,7 @@ def health():
     """Verifica se API está rodando"""
     return {"status": "ok"}
 
-@app.post("/calcular/{metodo}", response_model=ResultadoCalculo, status_code=200)
+@app.post("/calcular/{metodo}", response_model=ResultadoCalculo)
 def calcular(metodo: str, premissas: dict):
     """
     Calcula preço teto com base em um modelo e suas premissas
@@ -29,10 +28,20 @@ def calcular(metodo: str, premissas: dict):
     try:
         metodo_enum = MetodoPreco(metodo)
     except ValueError:
-        raise HTTPException(400, "Método inválido")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Método inválido. Opções: {', '.join([m.value for m in MetodoPreco])}"
+        )
 
+    try:
+        PremissasClass = PremissasFactory.get(metodo_enum)
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e)
+        )
     # Valida contra Pydantic
-    premissas_validadas = PremissasFactory(**premissas)
+    premissas_validadas = PremissasClass(**premissas)
 
     calculadora = CalculadoraTetoDePreco()
     resultado = calculadora.calcular(metodo=metodo_enum, premissas=premissas_validadas)
--- src/site_backend/models.py
+++ src/site_backend/models.py
@@ -16,30 +16,30 @@ class PremissasCalculo(BaseModel):
     pass
 
 class PremissasGrahamNumber(BaseModel):
-    lpa: float = Field(..., gt=0, description="ex: 5.0")
-    vpa: float = Field(..., gt=0, description="ex: 0.10")
-    pl_limite: float = Field(default=15.0,  gt=0, description="ex: 10.0") 
-    pvp_limite: float = Field(default=1.5,  gt=0, description="ex: 1.0") 
+    lpa: float = Field(..., gt=0, examples=[5.0, 1.69], description="ex: 5.0")
+    vpa: float = Field(..., gt=0, examples=[10.9, 15.2], description="ex: 10.9")
+    pl_limite: float = Field(default=15.0,  gt=0, examples=[10.0], description="ex: 10.0") 
+    pvp_limite: float = Field(default=1.5,  gt=0, examples=[1.0], description="ex: 1.0") 
 
 class PremissasGrahamFormula(BaseModel):
-    lpa: float = Field(..., gt=0, description="ex: 5.0")
-    growth: float = Field(..., ge=0, le=1, description="ex: 0.30 (30%)")
-    yield_base: float = Field(..., ge=0, description="ex: 0.044 (4,4%)")
-    yield_atual: float = Field(..., ge=0,description="ex: 0.18 (18%)")
+    lpa: float = Field(..., gt=0, examples=[5.0, 1.69], description="ex: 5.0")
+    growth: float = Field(..., ge=0, le=1, examples=[0.3], description="ex: 0.30 (30%)")
+    yield_base: float = Field(..., ge=0, examples=[0.055], description="ex: 0.055 (5,5%)")
+    yield_atual: float = Field(..., ge=0, examples=[0.0783], description="ex: 0.0783 (7,83%)")
 
 class PremissasBazin(BaseModel):
-    dpa: float = Field(..., gt=0, description="ex: (R$) 1.1")
-    dy_minimo: float = Field(default=0.06, description="ex: 0.08 (8%)")
+    dpa: float = Field(..., gt=0, examples=[1.36, 2.2], description="ex: (R$) 1.1")
+    dy_minimo: float = Field(default=0.06, examples=[0.08, 0.1], description="ex: 0.08 (8%)")
 
 class PremissasGordon(BaseModel):
-    dpa: float = Field(..., gt=0, description="ex: (R$)1.1")
-    retorno: float = Field(..., description="ex: 0.08 (8%)")
-    growth: float = Field(..., ge=0, le=1, description="ex: 0.05 (5%)")
+    dpa: float = Field(..., gt=0, examples=[1.36, 2.2], description="ex: (R$)1.1")
+    retorno: float = Field(..., examples=[0.08, 0.1], description="ex: 0.08 (8%)")
+    growth: float = Field(..., ge=0, le=1, examples=[0.05], description="ex: 0.05 (5%)")
 
 class PremissasFiiNtnb(BaseModel):
-    dpa: float = Field(..., gt=0, description="ex: (R$)1.1")
-    retorno: float = Field(..., description="ex: 0.02 (2%)")
-    ntnb: float = Field(..., ge=0, le=1, description="ex: 0.065 (6,5%)")
+    dpa: float = Field(..., gt=0, examples=[1.36, 2.2], description="ex: (R$)1.1")
+    retorno: float = Field(..., examples=[0.02], description="ex: 0.02 (2%)")
+    ntnb: float = Field(..., ge=0, le=1, examples=[0.065], description="ex: 0.065 (6,5%)")
 
 class ResultadoCalculo(BaseModel):
     preco_teto: float
